8c0b726d608d6e8cdc53cc1ee7bf2563a123cf94
Adam J Calhoun <adam.calhoun@gmail.com>
2020-03-11 10:38:58 -0400

    Added per capita

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 COVID-19 generally follows a similar growth pattern. The below is updated on March 11, 2020 and shows the number of cases in the days since >200 cases were reported in that country. The USA looks to be 7-10 days behind Italy.
 
 ![Number of cases, in days since >200 cases](031120_cases.png)
+
+If you look at cases per capita, the USA is growing just as quickly but is not nearly as saturated (so far).
+
+![Number of cases, in days since >200 cases](031120_cases_percapita.png)
--- covid.py
+++ covid.py
@@ -32,7 +32,7 @@ def plot_confirmed_cases(country_info,country_list):
 
 # plot the number of confirmed cases shifted by days since THRESH cases
 # (OPTIONAL: date_offset lets you start plotting a few days before this convergence)
-def plot_confirmed_cases_by_thresh(country_info,country_list,case_thresh,date_offset=0):
+def plot_confirmed_cases_by_thresh(country_info,country_list,case_thresh,date_offset=0,per_capita=None):
 	start_date = [np.where(ci > case_thresh)[0][0] + date_offset for ci in country_info.T]
 	end_date = country_info.shape[0]
 
@@ -43,22 +43,31 @@ def plot_confirmed_cases_by_thresh(country_info,country_list,case_thresh,date_of
 	for ind,info in enumerate(country_info.T):
 		# print(end_date - start_date[ind])
 		# print(info)
-		shaped_data[ind,:end_date - start_date[ind]] = info[start_date[ind]:]
+		if per_capita is None:
+			shaped_data[ind,:end_date - start_date[ind]] = info[start_date[ind]:]
+		else:
+			shaped_data[ind,:end_date - start_date[ind]] = info[start_date[ind]:]/per_capita[ind]
 
 
 	plt.semilogy(range(date_offset,end_date - np.min(start_date)),shaped_data.T, marker='o')
 	# sns.lineplot(shaped_data.T)
 	plt.legend(country_list)
 	plt.xlabel('days since ' + str(case_thresh) + ' cases reported')
-	plt.ylabel('number of confirmed COVID-19 cases')
+	if per_capita is None:
+		plt.ylabel('number of confirmed COVID-19 cases')
+	else:
+		plt.ylabel('number of confirmed COVID-19 cases per capita (millions)')
+	plt.title('growth in COVID-19 cases as of 11-03-20')
 	plt.show()
 
 if __name__=='__main__':
 	country_list = ['Italy','Iran (Islamic Republic of)','US','Germany','France','Spain','Republic of Korea']
+	country_pop = [60.48,81.16,327.2,82.79,66.99,46.66,51.47]
 	df = load_data()
 
 	country_info = get_country_data(df,country_list)
 
 	# plot_confirmed_cases(country_info,country_list)
-	plot_confirmed_cases_by_thresh(country_info,country_list,case_thresh=200,date_offset=-3)
+	# plot_confirmed_cases_by_thresh(country_info,country_list,case_thresh=200,date_offset=-3)
+	plot_confirmed_cases_by_thresh(country_info,country_list,case_thresh=200,date_offset=-3,per_capita=country_pop)
 
